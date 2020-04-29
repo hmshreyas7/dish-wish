ec23dbae986ec47196ffff0a58a581f228dce34c
hmshreyas7 <hmshreyas7@gmail.com>
2020-04-28 21:33:08 -0700

    Add material toolbar and implement floating action button here

--- app/src/main/java/com/example/dishwish/MainActivity.java
+++ app/src/main/java/com/example/dishwish/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.dishwish;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TableLayout;
+import android.view.View;
 
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +18,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Set material toolbar to act as actionbar
+        MaterialToolbar toolbar = findViewById(R.id.topAppBar);
+        setSupportActionBar(toolbar);
+
         // Define a custom adapter to power a view pager with fragments
         ViewPager viewPager = findViewById(R.id.pager);
         CustomPagerAdapter customPagerAdapter = new CustomPagerAdapter(this, getSupportFragmentManager());
@@ -23,5 +30,15 @@ public class MainActivity extends AppCompatActivity {
         // Use the view pager to power a tab layout
         TabLayout tabLayout = findViewById(R.id.tab_layout);
         tabLayout.setupWithViewPager(viewPager);
+
+        FloatingActionButton floatingActionButton =
+                (FloatingActionButton) findViewById(R.id.floating_action_button);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AddDishActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
