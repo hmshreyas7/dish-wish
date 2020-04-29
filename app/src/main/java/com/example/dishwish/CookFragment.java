bd27acb3a784a610cafd19b361de20b7d10809ba
hmshreyas7 <hmshreyas7@gmail.com>
2020-04-28 21:36:55 -0700

    Rearrange imports

--- app/src/main/java/com/example/dishwish/CookFragment.java
+++ app/src/main/java/com/example/dishwish/CookFragment.java
@@ -1,13 +1,11 @@
 package com.example.dishwish;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
 
 /**
  * A simple {@link Fragment} subclass.
