0630a61f09526102a36b730b33c239ee6b2c976e
hmshreyas7 <hmshreyas7@gmail.com>
2020-05-03 08:56:28 -0700

    Create "done" option and build item lists for dropdown menus

--- app/src/main/java/com/example/dishwish/AddDishActivity.java
+++ app/src/main/java/com/example/dishwish/AddDishActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 import com.google.android.material.appbar.MaterialToolbar;
 
@@ -23,5 +27,36 @@ public class AddDishActivity extends AppCompatActivity {
 
         // Enable the Up button
         ab.setDisplayHomeAsUpEnabled(true);
+
+        // Build list of dish types
+        AutoCompleteTextView dishType = findViewById(R.id.dish_type);
+        String[] dishTypeOptions = new String[]{getString(R.string.savory), getString(R.string.sweet)};
+        buildMenuItems(dishType, dishTypeOptions);
+
+        // Build list of categories
+        AutoCompleteTextView category = findViewById(R.id.category);
+        String[] categoryOptions = new String[]{getString(R.string.cook), getString(R.string.eat)};
+        buildMenuItems(category, categoryOptions);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu with custom menu resource file
+        getMenuInflater().inflate(R.menu.app_bar_options, menu);
+        return true;
+    }
+
+    /**
+     * Builds a list of items for a dropdown menu.
+     *
+     * @param menu  the view that displays the selected item
+     * @param items the list of items to be displayed
+     */
+    public void buildMenuItems(AutoCompleteTextView menu, String[] items) {
+        menu.setText(items[0], false);
+        menu.setInputType(InputType.TYPE_NULL);
+        ArrayAdapter<String> menuAdapter = new ArrayAdapter<>(getApplicationContext(),
+                R.layout.dropdown_menu_items, items);
+        menu.setAdapter(menuAdapter);
     }
 }
