ed9ce6351d8aa7ee2abdf02d33f90aa8e9f087e1
hmshreyas7 <hmshreyas7@gmail.com>
2020-05-16 11:27:23 -0700

    Add constants for content URI

--- app/src/main/java/com/example/dishwish/data/DishContract.java
+++ app/src/main/java/com/example/dishwish/data/DishContract.java
@@ -1,8 +1,29 @@
 package com.example.dishwish.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public final class DishContract {
+
+    /**
+     * The "Content authority" is a name for the entire content provider, similar to the
+     * relationship between a domain name and its website.  A convenient string to use for the
+     * content authority is the package name for the app, which is guaranteed to be unique on the
+     * device.
+     */
+    public static final String CONTENT_AUTHORITY = "com.example.dishwish";
+    /**
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider.
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    /**
+     * Possible path (appended to base content URI for possible URI's)
+     * For instance, content://com.example.dishwish/dishes/ is a valid path for
+     * looking at dish data. content://com.example.dishwish/menu/ will fail,
+     * as the ContentProvider hasn't been given any information on what to do with "menu".
+     */
+    public static final String PATH_DISHES = "dishes";
 
     // To prevent someone from accidentally instantiating the contract class,
     // make the constructor private.
@@ -11,6 +32,12 @@ public final class DishContract {
 
     /* Inner class that defines the table contents */
     public static class DishEntry implements BaseColumns {
+
+        /**
+         * The content URI to access the dish data in the provider
+         */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_DISHES);
+
         // Name of the table for dishes
         public static final String TABLE_NAME = "dishes";
 
