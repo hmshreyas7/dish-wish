9d47364a76f58107e3c67821b420d01c7cc87c40
hmshreyas7 <hmshreyas7@gmail.com>
2020-05-28 23:33:41 -0700

    Fix issue with displaying "done"

--- app/src/main/java/com/example/dishwish/AddDishActivity.java
+++ app/src/main/java/com/example/dishwish/AddDishActivity.java
@@ -81,11 +81,8 @@ public class AddDishActivity extends AppCompatActivity implements LoaderManager.
         // Inflate the menu with custom menu resource file
         getMenuInflater().inflate(R.menu.app_bar_options, menu);
 
-        // Disable "done" by default
-        final MenuItem doneItem = menu.findItem(R.id.done);
-        doneItem.setVisible(false);
-
         // Handle events based on how the dish title changes
+        final MenuItem doneItem = menu.findItem(R.id.done);
         dishTitleText = findViewById(R.id.dish_title_text);
         final int maxDishTitleLength = 50;
         dishTitleText.addTextChangedListener(new TextWatcher() {
